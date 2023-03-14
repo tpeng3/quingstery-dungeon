77fb79cf5ba8a51e190f92559933b32c92698e65
cae <caebbage@users.noreply.github.com>
2023-03-13 22:16:49 -0500

    checkbox clarified, speech bubble added
    label light variation added, fixed display for top bar's gald count

--- prefabs/UI/TopBar/TopBar.gd
+++ prefabs/UI/TopBar/TopBar.gd
@@ -4,7 +4,7 @@ extends Control
 
 # Called when the node enters the scene tree for the first time.
 func _process(delta):
-	$UpperInfo/HSplitContainer/GaldCounter/GaldLabel.text = \
-		str(Inventory.gald) + " Gald"
+	$UpperInfo/HSplitContainer/GaldCounter/FlowContainer/GaldLabel.text = \
+		str(Inventory.gald) + "  "
 	$UpperInfo/HSplitContainer/InventoryAmt/InventoryLabel.text = \
 		str(Inventory.get_inv_count()) + "/" + str(Inventory.max)
