689967650af2577144e0bab6de844c0022eddb46
Tina Peng <tpeng3@ucsc.edu>
2023-03-27 22:41:28 -0700

    updated map pip previews

--- prefabs/MapButton/MapButton.gd
+++ prefabs/MapButton/MapButton.gd
@@ -5,14 +5,14 @@ var bip:Texture2D = load("res://assets/ui/mapbip.png")
 @export var scene:String 
 @export var preview:Texture2D
 @export var skipPreview:bool = false
-@export var mapDesc:String
+@export_multiline var mapDesc:String
 @onready var Camera = get_parent().get_parent().get_node("Camera2D")
 
 signal map_focused
 
 func _on_focus_entered():
 	if not skipPreview:
-		$TextureButton.texture_normal = preview
+		$Sprite.texture = preview
 		$AnimationPlayer.play("scale")
 	Camera.focused_location = self
 	z_index = 1
@@ -22,6 +22,6 @@ func _on_focus_exited():
 	if not skipPreview:
 		$AnimationPlayer.play_backwards("scale")
 		await $AnimationPlayer.animation_finished
-		$TextureButton.texture_normal = bip
+		$Sprite.texture = bip
 	self.scale = Vector2(1, 1)
 	z_index = 0
