e8a505422e18ad2667e6a2808b11ca69e37ca6e8
cae <caebbage@users.noreply.github.com>
2023-04-13 03:56:46 -0500

    status menu formatting, progress bar code

--- prefabs/UI/StatusMenu/ClimbDisplay.gd
+++ prefabs/UI/StatusMenu/ClimbDisplay.gd
@@ -3,7 +3,7 @@ extends MarginContainer
 var quingPosition = 365
 
 func _ready():
-	$ClimbProgress.max_value = Global.CheckpointType.TOP
+	update_progress()
 	
 func update_progress():
 	$ClimbProgress/Throat/Label.text = str(Global.CheckpointType.THROAT) if \
@@ -24,7 +24,31 @@ func update_progress():
 	$ClimbProgress/Peak.region_rect = Rect2(16, 72 - ( 18*4), 18, 18) if \
 		Global.currentCheckpoint > Global.CheckpointType.PEAK else Rect2(16, 72, 18, 18)
 	
-	$ClimbProgress/QuingProgress/Label.text = str(Global.currentFloor)
-	$ClimbProgress/QuingProgress.position = Vector2(26, quingPosition - (1.0 * Global.currentFloor / Global.CheckpointType.TOP) * quingPosition)
-	$ClimbProgress.value = Global.currentFloor
 	
+	$ClimbProgress/QuingProgress/Label.text = str(Global.currentFloor)
+	
+	if (Global.currentFloor == Global.CheckpointType.TOP):
+		$ClimbProgress.value = 100
+		
+	elif (Global.currentFloor >= Global.CheckpointType.PEAK):
+		# the current amount of steps past the previous checkpoint
+		# multiplied by 25, the length of the next section of the bar
+		# divided by the amt of steps between this and next checkpoint
+		$ClimbProgress.value = 75.0 \
+		+ ((Global.currentFloor - Global.CheckpointType.PEAK) * 25.0 \
+		/ (Global.CheckpointType.TOP - Global.CheckpointType.PEAK))
+		
+	elif (Global.currentFloor >= Global.CheckpointType.STEPS):
+		$ClimbProgress.value = 50 \
+		+ ((Global.currentFloor - Global.CheckpointType.STEPS) * 25.0 \
+		/ (Global.CheckpointType.PEAK - Global.CheckpointType.STEPS))
+		
+	elif (Global.currentFloor >= Global.CheckpointType.RIVER):
+		$ClimbProgress.value = 25 \
+		+ ((Global.currentFloor - Global.CheckpointType.RIVER ) * 25.0 \
+		/ (Global.CheckpointType.STEPS - Global.CheckpointType.RIVER))
+	else:
+		$ClimbProgress.value = (Global.currentFloor * 25.0 / Global.CheckpointType.RIVER)
+		
+	# sets bar position by reversing value and multiplying to scale to height
+	$ClimbProgress/QuingProgress.position = Vector2(26, (100 - $ClimbProgress.value) * 3.6) 
